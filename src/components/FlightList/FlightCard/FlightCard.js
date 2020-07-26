e233b8a9e2fd19818c744f5c6973227ae769c68b
3103ski <bjastski@gmail.com>
2020-07-25 18:54:32 -0700

    connects flight list api to UI for listing

--- src/components/FlightList/FlightCard/FlightCard.js
+++ src/components/FlightList/FlightCard/FlightCard.js
@@ -5,31 +5,34 @@ const FlightCard = (props) => {
 	return (
 		<div className={s.FlightCardContainer}>
 			<div className={s.CardCol}>
-				<h2>Flight#: CI5256</h2>
-				<p>Parking: R-12</p>
-				<p>Flight Routing: TPE-ANC-ATL-DFW</p>
-				<p>Flight Coordinator: Tj</p>
+				<h2>Flight#: {props.flightNumber ? props.flightNumber : '----'}</h2>
+				<p>Parking: {props.parking ? props.parking : '----'}</p>
+				<p>Flight Routing: {props.routing ? props.routing : '----'}</p>
+				<p>
+					Flight Coordinator:{' '}
+					{props.flightCoordinator ? props.flightCoordinator : 'Not Scheduled'}
+				</p>
 			</div>
 			<div className={s.Divider}></div>
 			<div className={s.CardCol}>
 				<h2>STA</h2>
-				<p>0005</p>
+				<p>{props.scheduledTOA ? props.scheduledTOA : '----'}</p>
 				<h2>STD</h2>
-				<p>0105</p>
+				<p>{props.scheduledTOD ? props.scheduledTOD : '----'}</p>
 			</div>
 			<div className={s.Divider}></div>
 			<div className={s.CardCol}>
 				<h2>ETA</h2>
-				<p>0105</p>
+				<p>{props.estimatedTOA ? props.estimatedTOA : '----'}</p>
 				<h2>ETD</h2>
-				<p>0205</p>
+				<p>{props.estimatedTOD ? props.estimatedTOD : '----'}</p>
 			</div>
 			<div className={s.Divider}></div>
 			<div className={s.CardCol}>
 				<h2>ATA</h2>
-				<p>0100</p>
+				<p>{props.actualTOA ? props.actualTOA : '----'}</p>
 				<h2>ATD</h2>
-				<p>0205</p>
+				<p>{props.actualTOD ? props.actualTOD : '----'}</p>
 			</div>
 			<div className={s.Divider}></div>
 			<div className={(s.CardCol, s.BtnCol)}>
--- src/components/FlightList/FlightList.js
+++ src/components/FlightList/FlightList.js
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import s from './FlightList.module.scss';
 
 // Components
 import Flight from './FlightCard/FlightCard';
 
-class FlightList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			flightsAreLoaded: false,
-		};
-	}
-	render() {
-		return (
-			<div>
-				<Flight></Flight>
-				<Flight></Flight>
-				<Flight></Flight>
-			</div>
+function loadFlights(callback) {
+	const xhr = new XMLHttpRequest();
+	const method = 'GET';
+	const url = 'http://127.0.0.1:8000/api/flights/';
+	const responseType = 'json';
+
+	xhr.responseType = responseType;
+	xhr.open(method, url);
+	xhr.onload = function () {
+		callback(xhr.response, xhr.status);
+		console.log('This is what we got back: ', xhr.response);
+	};
+	xhr.onerror = function () {
+		console.log(xhr.response);
+		callback(
+			{ message: 'There was an error retrieving flights from the database.' },
+			400
 		);
-	}
+	};
+	xhr.send();
 }
+
+const FlightList = (props) => {
+	const [flights, setFlights] = useState([]);
+	useEffect(() => {
+		const flightCallback = (response, status) => {
+			if (status === 200) {
+				setFlights(response);
+			} else if (status === 400) {
+				console.log(response, `\nStatus: ${status}`);
+			}
+		};
+		loadFlights(flightCallback);
+	}, []);
+
+	return (
+		<div>
+			{flights.map((flight, index) => {
+				return (
+					<Flight
+						key={index}
+						flightNumber={flight.flightNumber}
+						parking={flight.parking}
+						routing={flight.routing}
+						flightCoordinator={flight.flightCoordinator}
+						scheduledTOA={flight.scheduledTOA}
+						scheduledTOD={flight.scheduledTOD}
+						estimatedTOA={flight.estimatedTOA}
+						estimatedTOD={flight.estimatedTOD}
+						actualTOA={flight.actualTOA}
+						actualTOD={flight.actualTOD}></Flight>
+				);
+			})}
+		</div>
+	);
+};
 
 export default FlightList;
